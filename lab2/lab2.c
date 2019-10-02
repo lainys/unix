1670e89e25bfd5725ddfed9ddc9554b1ca854020
lainys <lainys@localhost.localdomain>
2019-10-03 02:47:58 +0300

    lab2

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,7 +1,3 @@
-/*Это простейший модуль PAM*/
-
-// Включаем необходимые заголовочные файлы.
-
 #include <security/pam_modules.h>
 #include <stdarg.h>
 #include <time.h>
@@ -31,12 +27,10 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t * pamh, int flags
         x=random()%100;
 
         retval = pam_get_user(pamh, &name, "login: ");
-
         
         struct pam_conv *conv;
         struct pam_message *pmsg[3],msg[3];
         struct pam_response *response;
-
 
         retval = pam_get_item( pamh, PAM_CONV, (const void **) &conv );
 
@@ -45,8 +39,7 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t * pamh, int flags
         msg[0].msg=malloc(100);
         snprintf(msg[0].msg,60,"Password: (%d:%d,%d)",mytm->tm_hour,mytm->tm_min,x);
 
-        retval = conv->conv(1, ( const struct pam_message ** ) pmsg
-                            , &response, conv->appdata_ptr);
+        retval = conv->conv(1, ( const struct pam_message ** ) pmsg, &response, conv->appdata_ptr);
 
         int hour = mytm->tm_hour;
         int min = mytm->tm_min;
@@ -62,7 +55,6 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t * pamh, int flags
         return PAM_SUCCESS;
 }
 
-
 /*
  * The only thing _pam_set_credentials_unix() does is initialization of
  * UNIX group IDs.
@@ -74,8 +66,7 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t * pamh, int flags
    Идентификаторов групп... короче в данном случае это нам совершенно не нужно
  */
 
-PAM_EXTERN int pam_sm_setcred(pam_handle_t * pamh, int flags
-                              ,int argc, const char **argv)
+PAM_EXTERN int pam_sm_setcred(pam_handle_t * pamh, int flags, int argc, const char **argv)
 {
         unsigned int ctrl;
         int retval;
@@ -85,8 +76,7 @@ PAM_EXTERN int pam_sm_setcred(pam_handle_t * pamh, int flags
         return retval;
 }
 
-// Это определение необходимо для статической линковки модулей PAM в приложения
-//х.
+// Это определение необходимо для статической линковки модулей PAM в приложения х.
 #ifdef PAM_STATIC
 struct pam_module _pam_unix_auth_modstruct = {
     "pam_mypam",
